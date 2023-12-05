8d54a7c2f6910adbd8d3fd70be8c6be12f64b200
= <=>
2023-12-05 21:57:41 +0400

    azn

--- text.js
+++ text.js
@@ -11,6 +11,6 @@ convert(azn, usdCurr);
 convert(azn, eurCurr);
 convert(azn, tryCurr);
 
-let netice = `${azn} = ${usdCurr * 1.7.toFixed()} Dollar, ${azn} = ${eurCurr  * 1.8.toFixed()} Euro, ${azn} = ${tryCurr * 11} Turk lirasi`
+let netice = `${azn}AZN = ${usdCurr * 1.7.toFixed()} Dollar, ${azn} = ${eurCurr  * 1.8.toFixed()} Euro, ${azn} = ${tryCurr * 11} Turk lirasi`
 
 document.getElementById('valy').innerHTML = netice
